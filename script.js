ecc3edd3fc6ffef355088705bf9fac23a0ef964a
Reshmi Sera Alex <reshmi.alex@gmail.com>
2024-08-21 21:55:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.write("Hello World<br>")
+document.write("Hello World")
+console.log("This is a message");
+console.warn("Warning")
+console.error("error message")
+//single line comment
+/*Multi
+line comment*/
+
+let person="Annie"
+console.log(person)
+
+var number=19
+console.log(number)
